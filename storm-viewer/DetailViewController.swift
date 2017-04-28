59ee753f5088cdae328d1ee53c6a1d9634475904
Noah Berman <noah@noahberman.org>
2017-04-28 16:53:18 -0500

    fix scaling and add fullscreen

--- storm-viewer/DetailViewController.swift
+++ storm-viewer/DetailViewController.swift
@@ -23,6 +23,16 @@ class DetailViewController: UIViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
